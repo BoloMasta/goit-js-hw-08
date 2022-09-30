9408926b1e5a6f62ceefd7663f7a1ec9db2bcf2c
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-09-30 20:48:38 +0200

    main

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const bolek = 'adamiec';
+// PRZYPISANIE KONTENERA GALERII DO ZMIENNEJ
+const galleryContainer = document.querySelector('.gallery');
 
+// STWORZENIE KODU HTML GALERII
+const markup = galleryItems
+  .map(
+    image =>
+      `
+       <a class="gallery__item" href="${image.original}">
+        <img
+          class="gallery__image"
+          src="${image.preview}"
+          alt="${image.description}"
+        />
+      </a>
+    `
+  )
+  .join('');
+
+// WSTAWIENIE GALERII DO KONTENERA
+galleryContainer.insertAdjacentHTML('afterbegin', markup);
+
+// DODANIE DO GALERII BIBLIOTEKI SimpleLightbox
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,46 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const licznik = document.getElementById('licznik');
+
+const onPlay = function (data) {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+      console.log('LOCAL: ' + localStorage.getItem('videoplayer-current-time'));
+      licznik.innerHTML = localStorage.getItem('videoplayer-current-time');
+    })
+    .catch(function (error) {
+      console.log('ERROR');
+    });
+};
+
+player.on('play', onPlay);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(
+          'the time was less than 0 or greater than the video’s duration'
+        );
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
