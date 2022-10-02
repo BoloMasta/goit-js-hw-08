c266f3c5043d31d97d9c541dc6c3cfa00baf1ac7
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-10-02 23:40:16 +0200

    main

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
 
+const type = () => {
+  let result = { email: input.value, message: textarea.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(result));
+};
+
+// lodash.throttle
+var throttle = require('lodash.throttle');
+var throttled = throttle(type, 500, { trailing: false });
+
+form.addEventListener('input', event => throttled());
+
+try {
+  var data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  input.value = data.email;
+  textarea.value = data.message;
+} catch (error) {}
+
+// SUBMIT
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+  input.value = '';
+  textarea.value = '';
+});
